3ccec2dc2f583f5fd0eb8d0f89cb97a1ac8ae243
Bryan Sattler <bsattler@gmail.com>
2013-12-09 20:33:44 -0500

    First commit and README update

---
+++ README.md
@@ -0,0 +1,3 @@
+# RubyTuts
+
+A web application that allows users to create public and private Markdown-based wikis where people can learn about Ruby on Rails.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubytuts::Application.config.secret_token = '2c1e99a2da65a3c48276c904c3f245b59dc4a593180b3fd55f0598acda5852b7aaa850d37d327d9881a1ec05abb6b4db86219f1ea3d94cb582c57b444689f266'
