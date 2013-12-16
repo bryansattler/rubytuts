0f20b765aec4379b2ee4b6e1898146f5eb5a6a0a
Bryan Sattler <bsattler@gmail.com>
2013-12-15 19:06:54 -0500

    Installed devise for user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rubytuts::Application.routes.draw do
 
+  devise_for :users
+
   resources :wikis
 
   match "about" => 'welcome#about', via: :get
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131215211919) do
+ActiveRecord::Schema.define(:version => 20131215234132) do
 
   create_table "comments", :force => true do |t|
     t.text     "body"
@@ -21,6 +21,29 @@ ActiveRecord::Schema.define(:version => 20131215211919) do
   end
 
   add_index "comments", ["wiki_id"], :name => "index_comments_on_wiki_id"
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0,  :null => false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
   create_table "wikis", :force => true do |t|
     t.string   "title"
