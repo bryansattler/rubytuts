d4a4427933c3e852f29ef99e2f65293c2bc2b245
Bryan Sattler <bsattler@gmail.com>
2013-12-15 20:16:46 -0500

    Associated users and wikis

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131215234132) do
+ActiveRecord::Schema.define(:version => 20131216010109) do
 
   create_table "comments", :force => true do |t|
     t.text     "body"
@@ -50,6 +50,9 @@ ActiveRecord::Schema.define(:version => 20131215234132) do
     t.text     "body"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
+
+  add_index "wikis", ["user_id"], :name => "index_wikis_on_user_id"
 
 end
